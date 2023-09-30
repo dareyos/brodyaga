30314bf06d7ee4851cfe90c4bd5992455b6a9f86
Dasha <dashabella1@gmail.com>
2023-09-30 14:36:28 +0500

    aaaaaa

--- bin/brodyaga.dart
+++ bin/brodyaga.dart
@@ -1,13 +1,35 @@
 import 'package:dio/dio.dart';
 import 'models/prodResp/prodResp.dart';
+import 'models/product/product.dart';
 
 void main(List<String> arguments) async {
   Dio client = Dio();
   String url = 'http://dummyjson.com/products';
 
   Response<dynamic> response = await client.get(url);
-  
   ProdResp data = ProdResp.fromJson(response.data);
+ 
+  List<Product> productsMassive = data.products;
+  List<Product> resultMassive = [];
+  String textResult = '';
+  for (Product element in productsMassive) {
+    if (element.brand == "Apple"){
+      resultMassive.add(element);
+    }
+  }
+  resultMassive = productMassive.where((el)=> el.brand == "Apple").toList();
 
-  print(data.total);
+  // int count = 0;
+  // for (var element in data.products) {
+  //   if (element.brand == "Apple") {
+  //     count++;
+  //   }
+  // }
+  
+  //print(count);
+
+}
+
+bool filterCondition(Product product) {
+  return product.brand == "Apple";
 }
--- bin/models/prodResp/prodResp.dart
+++ bin/models/prodResp/prodResp.dart
@@ -1,11 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import '../product/product.dart';
 part 'prodResp.freezed.dart';
 part 'prodResp.g.dart';
 
 @freezed
 class ProdResp with _$ProdResp {
   factory ProdResp({
-    @Default([]) List<dynamic> products, // значение по умолочанию
+    @Default([]) List<Product> products, // значение по умолочанию
     @Default(0) int total, 
     @Default(0) int skip,
     @Default(0) int limit,
